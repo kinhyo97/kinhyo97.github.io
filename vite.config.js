6fb35438828785a70a0b29ee14d8c0e6f1d030fc
kinhyo97 <v50509v@gmail.com>
2025-11-10 17:42:39 +0900

    git action 연동

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 이 부분 중요! (레포 이름이 kinhyo97.github.io이면 /)
+  base: "/", // ✅ 루트 페이지는 이렇게 해야 함!
 });
