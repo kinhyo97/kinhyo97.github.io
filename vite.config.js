99930137a89053b8e3d5ed86893a6c10909fcdcb
kinhyo97 <v50509v@gmail.com>
2025-11-10 17:03:31 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// ⚠️ 반드시 본인 주소로 base 설정
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/", // 이 부분 중요! (레포 이름이 kinhyo97.github.io이면 /)
+});
